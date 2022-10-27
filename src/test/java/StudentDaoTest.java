9792d3dafa070d6a76fd9f0677880eeb865baa76
Smilingny <230502771@qq.com>
2022-10-27 12:45:04 +0800

    测试添加爱好，查询所有信息两个方法

--- src/test/java/StudentDaoTest.java
+++ src/test/java/StudentDaoTest.java
@@ -1,8 +1,9 @@
 import com.edu.Dao.StudentDao;
+import com.edu.Entity.Hobby;
 import com.edu.Entity.Student;
 import org.junit.Test;
-
-import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 
 // 测试类
 public class StudentDaoTest {
@@ -19,6 +20,38 @@ public class StudentDaoTest {
         s.setAge(19);
         s.setPassword("xiaoli");
         // 调用Dao保存对象，写入MySQL
+        sDao.save(s);
+    }
+
+    // 创建学生并创建其爱好
+    @Test
+    public void createStuHobby(){
+        Student s=new Student();
+        s.setName("小李");
+        s.setAge(25);
+        s.setPassword("xiaoli");
+
+        // 创建泛型
+        List<Hobby> hobbyList = new ArrayList<>();
+        Hobby hobby1 = new Hobby();
+        hobby1.setName("乒乓球");
+        //绑定学生信息
+        hobby1.setStudent(s);
+
+        Hobby hobby2 = new Hobby();
+        hobby2.setName("篮球");
+        hobby2.setStudent(s);
+
+        Hobby hobby3 = new Hobby();
+        hobby3.setName("书法");
+        hobby3.setStudent(s);
+
+        // 将对象添加到泛型中
+        hobbyList.add(hobby1);
+        hobbyList.add(hobby2);
+        hobbyList.add(hobby3);
+        //绑定爱好信息
+        s.setHobbyList(hobbyList);
         sDao.save(s);
     }
 
@@ -46,5 +79,19 @@ public class StudentDaoTest {
     public void remove(){
         // 调用StudentDao根据学号删除信息
         sDao.delete(3L);
+    }
+
+    //查找所有用户
+    @Test
+    public void findAll(){
+        List<Student> studentList = new ArrayList<>();
+        // 调用StudentDao中方法查询所有用户并返回泛型列表
+        studentList = sDao.getAll();
+        Student s = new Student();
+        // 循环取出studentList中的对象并打印学生姓名
+        for (int i = 0; i < studentList.size(); i++) {
+            s= studentList.get(i);
+            System.out.println(s.getName());
+        }
     }
 }
